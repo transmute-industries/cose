7b24081835c54525cd7e87ea2873fde8d26a1214
Orie Steele <orie@transmute.industries>
2024-02-04 08:52:54 -0600

    update webpack sanity test

--- examples/webpack-example/index.js
+++ examples/webpack-example/index.js
@@ -5,7 +5,7 @@ const test = async () => {
   const k2 = await transmute.key.generate('ES256', 'application/jwk+json')
   const encoder = new TextEncoder();
   const decoder = new TextDecoder()
-  const signer = transmute.detached.signer({ secretKeyJwk: k2 })
+  const signer = transmute.detached.signer({ remote: transmute.crypto.signer({ secretKeyJwk: k2 }) })
   const message = '💣 test ✨ mesage 🔥'
   const payload = encoder.encode(message)
   const coseSign1 = await signer.sign({
@@ -13,7 +13,13 @@ const test = async () => {
     unprotectedHeader: new Map(),
     payload
   })
-  const verifier = transmute.detached.verifier({ publicKeyJwk: transmute.key.publicFromPrivate(k2) })
+  const verifier = transmute.detached.verifier({
+    resolver: {
+      resolve: () => {
+        return transmute.key.publicFromPrivate(k2)
+      }
+    }
+  })
   const verified = await verifier.verify({ coseSign1, payload })
   console.log(decoder.decode(verified));
 
