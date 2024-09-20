168b447a269ed646ce111a5938103eb275f541cc
Orie Steele <orie@transmute.industries>
2024-09-20 17:08:21 -0500

    cleaning

--- scripts/make-iana.sh
+++
@@ -1,7 +0,0 @@
-
-node scripts/make-iana-cose-algorithms.js; 
-node scripts/make-iana-cose-header-parameters.js; 
-node scripts/make-iana-cose-key-common-parameters.js; 
-node scripts/make-iana-cose-key-type-parameters.js;
-node scripts/make-iana-cose-key-type.js;
-node scripts/make-iana-cose-elliptic-curves.js;
