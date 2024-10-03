2fcaaedfbb5a5f8189ba84b9728017af079b54b2
Orie Steele <orie@transmute.industries>
2024-10-03 12:42:35 -0500

    cleaning

--- src/cbor/pretty/prettyCose.ts
+++ src/cbor/pretty/prettyCose.ts
@@ -1,7 +1,7 @@
 import * as cbor from 'cbor-web'
 import { prettyCoseSign1 } from './prettyCoseSign1'
 
-export const prettyCose = (data: Buffer) => {
+export const prettyCose = (data: ArrayBuffer) => {
   const decoded = cbor.decode(data)
   if (decoded.tag === 18) {
     return prettyCoseSign1(data)
